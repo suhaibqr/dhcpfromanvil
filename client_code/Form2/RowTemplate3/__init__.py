0e4e16f05bad9842b0d021024ed510c55575f1c1
skr.jor@gmail.com <skr.jor@gmail.com>
2024-03-25 13:56:14 +0000

    Created Form2.RowTemplate3; Edited Form2

--- client_code/Form2/RowTemplate3/__init__.py
+++ client_code/Form2/RowTemplate3/__init__.py
@@ -1,10 +1,7 @@
-from ._anvil_designer import Form2Template
+from ._anvil_designer import RowTemplate3Template
 from anvil import *
 
-# data keys will contain a button "reserve, revoke"
-# available IPs table, will contain a button "static"
-
-class Form2(Form2Template):
+class RowTemplate3(RowTemplate3Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
